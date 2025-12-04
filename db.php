ee4c622b618800633d910da57c5f3964ac39546b
Mate Saric <mate.saric@gl-it.ch>
2025-12-04 13:15:40 +0100

    deleted db.php because i dont use it

--- db.php
+++
@@ -1,6 +0,0 @@
-<?php
-$mysqli = new mysqli("localhost","root","","Kursverwaltung");
-if($mysqli->connect_error){
-    die(json_encode(["error"=>"DB-Verbindung fehlgeschlagen: ".$mysqli->connect_error]));
-}
-?>
